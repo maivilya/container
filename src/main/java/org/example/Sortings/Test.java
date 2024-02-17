b4b5f2cea174755d4b15fc88520ab9f0f629e927
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 00:14:53 +0500

    test HeapSort in Test.java

--- src/main/java/org/example/Sortings/Test.java
+++ src/main/java/org/example/Sortings/Test.java
@@ -2,7 +2,7 @@ package org.example.Sortings;
 
 import org.example.Sortings.CreateArray.CreateArray;
 import org.example.Sortings.MergeSort.MergeSort;
-
+import org.example.Sortings.HeapSort.HeapSort;
 import java.util.Arrays;
 
 public class Test {
@@ -10,7 +10,7 @@ public class Test {
     public static void main(String[] args) {
 
         testMergeSort();
-
+        testHeapSort();
 
     }
 
@@ -20,5 +20,12 @@ public class Test {
 
         int[] sortedArray = MergeSort.sort(array);
         System.out.println(Arrays.toString(sortedArray));
+    }
+
+    private static void testHeapSort() {
+        int[] array = CreateArray.create(12, -100, 100);
+        System.out.println(Arrays.toString(array));
+        HeapSort.sort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
