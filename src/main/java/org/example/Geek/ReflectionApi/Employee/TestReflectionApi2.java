754e9761207a06a2f01a1436d75b4aa0cc4f3e6d
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 02:08:49 +0500

    add getEmployeeMethodInfo method and test it

--- src/main/java/org/example/Geek/ReflectionApi/Employee/TestReflectionApi2.java
+++ src/main/java/org/example/Geek/ReflectionApi/Employee/TestReflectionApi2.java
@@ -1,11 +1,33 @@
 package org.example.Geek.ReflectionApi.Employee;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class TestReflectionApi2 {
 
     public static void main(String[] args) {
         getEmployeeInfo();
+        getEmployeeMethodInfo();
+    }
+
+    /**
+     * В этом методе получаем все методы у объекта employee
+     * и выводим их на экран
+     */
+    private static void getEmployeeMethodInfo() {
+        Employee employee = new Employee();
+        Method[] methods = employee.getClass().getMethods();
+        Method[] declaredMethods = employee.getClass().getDeclaredMethods();
+
+        System.out.println("Methods: ");
+        for (Method method : methods) {
+            System.out.println("method: " + method.getName());
+        }
+
+        System.out.println("\nDeclaredMethod: ");
+        for (Method method : declaredMethods) {
+            System.out.println("method: " + method.getName());
+        }
     }
 
     /**
