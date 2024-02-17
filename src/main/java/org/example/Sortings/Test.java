68f33bdfddeedfe91f4870ffb53041650b435087
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 00:02:54 +0500

    test MergeSort in Test

---
+++ src/main/java/org/example/Sortings/Test.java
@@ -0,0 +1,19 @@
+package org.example.Sortings;
+
+import org.example.Sortings.CreateArray.CreateArray;
+import org.example.Sortings.MergeSort.MergeSort;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int[] array = CreateArray.create(12, -100, 100);
+        System.out.println(Arrays.toString(array));
+
+        int[] sortedArray = MergeSort.sort(array);
+        System.out.println(Arrays.toString(sortedArray));
+
+    }
+}
