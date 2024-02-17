3e3d9833e29b30497da9da36786b5f825e068771
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 00:10:45 +0500

    add sort method in HeapSort.java

--- src/main/java/org/example/Sortings/HeapSort/HeapSort.java
+++ src/main/java/org/example/Sortings/HeapSort/HeapSort.java
@@ -3,6 +3,23 @@ package org.example.Sortings.HeapSort;
 public class HeapSort {
 
     /**
+     * Главный метод сортировки кучей
+     * @param array неотсортированный массив
+     */
+    public static void sort(int[] array) {
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, i, array.length);
+        }
+        for (int i = array.length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, 0, i);
+        }
+    }
+
+    /**
      * Разбиение дерева:
      * @param array   исходный массив
      * @param maxHeap корень массива(дерева)
