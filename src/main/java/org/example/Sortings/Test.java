c7c3a805cebe685e70bbad4ec44e5fe31eb058fe
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 00:05:11 +0500

    separate the mergeSortTest into a separate method

--- src/main/java/org/example/Sortings/Test.java
+++ src/main/java/org/example/Sortings/Test.java
@@ -9,11 +9,16 @@ public class Test {
 
     public static void main(String[] args) {
 
+        testMergeSort();
+
+
+    }
+
+    private static void testMergeSort() {
         int[] array = CreateArray.create(12, -100, 100);
         System.out.println(Arrays.toString(array));
 
         int[] sortedArray = MergeSort.sort(array);
         System.out.println(Arrays.toString(sortedArray));
-
     }
 }
