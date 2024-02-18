6e913e83d1e3e3e1a552869b95271cd08731e365
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 16:36:46 +0500

    add Apple.java daughter Fruit

---
+++ src/main/java/org/example/Geek/Generic/FruitAndBoxes/Fruits/Apple.java
@@ -0,0 +1,11 @@
+package org.example.Geek.Generic.FruitAndBoxes.Fruits;
+
+public class Apple implements Fruit{
+
+    private static final double WEIGHT = 1.23;
+
+    @Override
+    public double getWeight() {
+        return WEIGHT;
+    }
+}
