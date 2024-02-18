0160f73c8c00a2db78e65b4ba5f2b2e77a96a84e
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 16:36:55 +0500

    add Orange.java daughter Fruit

---
+++ src/main/java/org/example/Geek/Generic/FruitAndBoxes/Fruits/Orange.java
@@ -0,0 +1,12 @@
+package org.example.Geek.Generic.FruitAndBoxes.Fruits;
+
+public class Orange implements Fruit {
+
+    private static final double WEIGHT = 1.35;
+
+
+    @Override
+    public double getWeight() {
+        return WEIGHT;
+    }
+}
