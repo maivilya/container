b4fb66e722bb453c7c1ee4b32661923a4420cfcb
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 16:58:45 +0500

    added constants

--- src/main/java/org/example/Geek/ParadoxMontyHall/Program.java
+++ src/main/java/org/example/Geek/ParadoxMontyHall/Program.java
@@ -1,6 +1,19 @@
 package org.example.Geek.ParadoxMontyHall;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Program {
+
+    private static final Map<Integer, Boolean> STATISTIC_CHANGE_DOOR = new HashMap<>();
+    private static final Map<Integer, Boolean> STATISTIC_D0_NOT_CHANGE_DOOR = new HashMap<>();
+    private static final Random random = new Random();
+    private static final int DOORS_COUNT = 3;
+    private static final int DOT_GOAT = 0;
+    private static final int DOT_CAR = 1;
+    private static final int[] DOORS = new int[DOORS_COUNT];
+    private static final int ATTEMPTS = 1000;
 
     public static void main(String[] args) {
 
