6a8db1bb6938c3eb6059e3e407a3ed3e085f1d20
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 17:27:41 +0500

    test philosophers and forks

---
+++ src/main/java/org/example/Geek/Threads/PhilosophersAndForks/Test.java
@@ -0,0 +1,9 @@
+package org.example.Geek.Threads.PhilosophersAndForks;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Table table = new Table();
+        table.start();
+    }
+}
