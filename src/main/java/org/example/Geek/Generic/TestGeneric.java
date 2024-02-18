f081544281f7f9eed67fc4c558987bac52f8c712
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-18 16:51:09 +0500

    test Compare Arrays in TestGeneric

--- src/main/java/org/example/Geek/Generic/TestGeneric.java
+++ src/main/java/org/example/Geek/Generic/TestGeneric.java
@@ -1,16 +1,33 @@
 package org.example.Geek.Generic;
 
 import org.example.Geek.Generic.Calculator.Calculator;
+import org.example.Geek.Generic.CompareArrays.CompareArrays;
 import org.example.Geek.Generic.FruitAndBoxes.Fruits.*;
 import org.example.Geek.Generic.FruitAndBoxes.Box;
 
 public class TestGeneric {
 
     public static void main(String[] args) {
-
         testCalculator();
         testFruitsAndBoxes();
+        testCompareArrays();
+    }
 
+    private static void testCompareArrays() {
+        Integer[] integers0 = new Integer[]{1, 2, 3, 4, 5, 6, 7};
+        Integer[] integers1 = new Integer[]{1, 2, 3, 4, 5, 6, 7};
+        boolean compare0 = CompareArrays.compare(integers0, integers1);
+        System.out.println("compare = " + compare0);
+
+        String[] strings0 = new String[]{"bow", "cow", "rabbit", "wolf"};
+        String[] strings1 = new String[]{"cow", "rabbit", "wolf"};
+        boolean compare1 = CompareArrays.compare(strings0, strings1);
+        System.out.println("compare1 = " + compare1);
+
+        Double[] doubles = new Double[]{2.4, 17.7, 192.3, 15.0};
+        Object[] objects = new Object[]{1, "car", 26.1, 'T'};
+        boolean compare2 = CompareArrays.compare(doubles, objects);
+        System.out.println("compare2 = " + compare2);
     }
 
     private static void testFruitsAndBoxes() {
