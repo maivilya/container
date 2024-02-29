ce36f71261a1a2b993622ec3a5f96005b25ae202
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-02-29 21:20:37 +0500

    added the socket and failed(connection refused)

--- src/main/java/org/example/Geek/ClientServer/Client/ClientVersion1.java
+++ src/main/java/org/example/Geek/ClientServer/Client/ClientVersion1.java
@@ -1,15 +1,22 @@
 package org.example.Geek.ClientServer.Client;
 
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.io.IOException;
 
 public class ClientVersion1 {
 
     public static void main(String[] args) {
+        Socket clietnSocket;
         try {
             InetAddress address = InetAddress.getLocalHost();
-            System.out.println(address);
-        } catch (UnknownHostException exception) {
+            clietnSocket = new Socket(address, 4305);
+            System.out.println(clietnSocket.getInetAddress());
+            clietnSocket.close();
+        } catch (UnknownHostException  exception) {
+            System.out.println("Exception: " + exception.getMessage());
+        } catch (IOException exception) {
             System.out.println("Exception: " + exception.getMessage());
         }
     }
