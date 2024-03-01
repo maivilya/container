37c81537332d191e8890b879d9fc70eaf3283761
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-03-01 17:11:10 +0500

    added class Server

---
+++ src/main/java/org/example/Geek/ClientServer/Chat/Server/Server.java
@@ -0,0 +1,13 @@
+package org.example.Geek.ClientServer.Chat.Server;
+
+import java.net.ServerSocket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+}
