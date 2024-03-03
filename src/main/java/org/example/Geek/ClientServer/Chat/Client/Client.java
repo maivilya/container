cfdf02a2fa2c9a767d3256cf3ac1493709f01970
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-03-03 12:30:39 +0500

    added sendMessage method

--- src/main/java/org/example/Geek/ClientServer/Chat/Client/Client.java
+++ src/main/java/org/example/Geek/ClientServer/Chat/Client/Client.java
@@ -2,6 +2,7 @@ package org.example.Geek.ClientServer.Chat.Client;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -16,6 +17,25 @@ public class Client {
         try {
           reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
           writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException exception) {
+            closeResources(socket, reader, writer);
+        }
+    }
+
+    public void sendMessage() {
+        try{
+            writer.write(name);
+            writer.newLine();
+            writer.flush();
+
+            String message;
+            Scanner scanner = new Scanner(System.in);
+            while(socket.isConnected()) {
+                message = scanner.nextLine();
+                writer.write(name + ": " + message);
+                writer.newLine();
+                writer.flush();
+            }
         } catch (IOException exception) {
             closeResources(socket, reader, writer);
         }
