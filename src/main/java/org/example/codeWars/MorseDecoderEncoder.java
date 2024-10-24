35eff6c29ec453eb79042e9d8e9076df0da042ae
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-10-24 18:08:51 +0500

    added map morse alphabet

---
+++ src/main/java/org/example/codeWars/MorseDecoderEncoder.java
@@ -0,0 +1,51 @@
+package org.example.codeWars;
+
+import java.util.Map;
+
+public class MorseDecoderEncoder {
+
+    private static Map<String, String> morseAlphabet;
+
+    public static void main(String[] args) {
+        initializeMorseAlphabet();
+    }
+
+    private static void initializeMorseAlphabet() {
+        morseAlphabet.put("A", ".-");
+        morseAlphabet.put("B", "");
+        morseAlphabet.put("C", "");
+        morseAlphabet.put("D", "");
+        morseAlphabet.put("E", "");
+        morseAlphabet.put("F", "");
+        morseAlphabet.put("G", "");
+        morseAlphabet.put("H", "");
+        morseAlphabet.put("I", "");
+        morseAlphabet.put("J", "");
+        morseAlphabet.put("K", "");
+        morseAlphabet.put("L", "");
+        morseAlphabet.put("M", "");
+        morseAlphabet.put("N", "");
+        morseAlphabet.put("O", "");
+        morseAlphabet.put("P", "");
+        morseAlphabet.put("Q", "");
+        morseAlphabet.put("R", "");
+        morseAlphabet.put("S", "");
+        morseAlphabet.put("T", "");
+        morseAlphabet.put("U", "");
+        morseAlphabet.put("V", "");
+        morseAlphabet.put("W", "");
+        morseAlphabet.put("X", "");
+        morseAlphabet.put("Y", "");
+        morseAlphabet.put("Z", "");
+        morseAlphabet.put("1", "");
+        morseAlphabet.put("2", "");
+        morseAlphabet.put("3", "");
+        morseAlphabet.put("4", "");
+        morseAlphabet.put("5", "");
+        morseAlphabet.put("6", "");
+        morseAlphabet.put("7", "");
+        morseAlphabet.put("8", "");
+        morseAlphabet.put("9", "");
+        morseAlphabet.put("0", "");
+    }
+}
